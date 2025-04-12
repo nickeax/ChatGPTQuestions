120ff95ad331e0f7f65dfd19b03412ab5f6015b9
Nick Fletcher <nickeax@gmail.com>
2025-04-13 09:49:09 +1000

    Pasted question into Main.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿/*
+ 2. Lambda Expressions in C#
+Question: Create a list of integers from 1 to 100. 
+Use a lambda expression to filter the list to only include numbers divisible by 3 and 5, then calculate their sum.
+ */
+
