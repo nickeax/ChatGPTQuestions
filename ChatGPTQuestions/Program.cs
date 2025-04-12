0ab4342df478d18a1a4195789c7b20fea1884ea4
Nick Fletcher <nickeax@gmail.com>
2025-04-13 08:14:06 +1000

    Completed code, switch back to dev to add Log class that will be used across all answers.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿/*
+ 1. Using Delegates with LINQ
+Question: Write a method that takes a list of integers and a delegate to filter out the even numbers. Use the delegate to remove even numbers from the list.
+ */
+
+var testData = Utils.Mocking.GenerateRandomInts(50).ToList();
+var res = FilterByDelegate(testData, a => a % 2 == 0);
+
+static List<int> FilterByDelegate(List<int> input, Func<int, bool> filter)
+{
+    return [.. input.Where(filter)];
+}
