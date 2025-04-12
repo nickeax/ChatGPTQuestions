b5c280e4db791d63b769c6397e9e6149dd9d669a
Nick Fletcher <nickeax@gmail.com>
2025-04-13 09:54:42 +1000

    Completed exercise.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -4,3 +4,9 @@ Question: Create a list of integers from 1 to 100.
 Use a lambda expression to filter the list to only include numbers divisible by 3 and 5, then calculate their sum.
  */
 
+var testData = Enumerable.Range(1, 100);
+var res = testData
+    .Where(x => x % 3 == 0 && x % 5 == 0)
+    .Sum();
+
+Console.WriteLine("Sum of numbers divisible by 3 and 5 from 1 to 100: " + res);
