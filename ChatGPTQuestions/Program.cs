23b1781dc3f89e87bdcae967b9cbceb2e9020a08
Nick Fletcher <nickeax@gmail.com>
2025-04-13 12:03:50 +1000

    Add question, commit to switch back to dev to add support functionality in Mocking class.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,6 @@
 ﻿/*
- 
+ 6. Using async and await
+Question: Write an asynchronous method that downloads data from a URL and prints the first 100 characters. Use async and await keywords to implement this. 
+Simulate a delay using Task.Delay if you don't actually want to make a network request.
  */
+
