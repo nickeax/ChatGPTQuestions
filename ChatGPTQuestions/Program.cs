c9523d06db4d941b6e3ad37d71bfa400cfbfa28a
Nick Fletcher <nickeax@gmail.com>
2025-04-13 17:04:50 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,23 @@
 ﻿/*
- 
+ 12. Access Modifiers
+Question: Create a class Person with private fields FirstName and LastName. 
+Provide public methods to get and set these fields. 
+Make sure to use the correct access modifiers and demonstrate how the access modifiers work.
  */
+
+var p = new Person();
+
+p.AccessFirstName = "Ken";
+p.AccessLastName = "Carter";
+
+Console.WriteLine($"Person.FirstName: {p.AccessFirstName}");
+Console.WriteLine($"Person.LastName: {p.AccessLastName}");
+
+public class Person
+{
+    string FirstName;
+    string LastName;
+
+    public string AccessFirstName { get => FirstName; set => FirstName = value ?? ""; }
+    public string AccessLastName { get => LastName; set => LastName = value ?? ""; }
+}
