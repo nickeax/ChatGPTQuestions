50db32bb54eeecbc040af1bd4cc467c1b2ead2f2
Nick Fletcher <nickeax@gmail.com>
2025-04-13 16:52:31 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,61 @@
 ﻿/*
- 
+10. Events and Event Handlers
+Question: Create a class TemperatureSensor that raises an event when the temperature exceeds 30°C. 
+Create a method HandleTemperatureExceeded to handle this event and print a message when it's triggered. 
  */
+var monitor = new TemperatureMonitor();
+
+
+public class TemperatureEventArgs : EventArgs
+{
+    public int Temperature { get; set; }
+    public string Message { get; set; }
+}
+
+public class TemperatureMonitor
+{
+    public TemperatureMonitor()
+    {
+        var ts = new TemperatureSensor();
+        ts.DangerousTemperature += Notification;
+        ts.TemperatureChanged += Notification;
+
+        ts.BeginTemperatureIncrease(20, 30);
+    }
+
+    public void Notification(Object sender, TemperatureEventArgs temperatureEventArgs)
+    {
+        Console.WriteLine($"UPDATE: {temperatureEventArgs.Message}");
+    }
+}
+
+public class TemperatureSensor
+{
+    public event EventHandler<TemperatureEventArgs> TemperatureChanged;
+    public event EventHandler<TemperatureEventArgs> DangerousTemperature;
+
+    public void BeginTemperatureIncrease(int startingTemperature, int maxTemperature)
+    {
+        for (int i = startingTemperature; i < 2000; i++)
+        {
+            RaiseTemperatureChanged(i);
+            Thread.Sleep(300);
+            if (i >= maxTemperature)
+            {
+                RaiseDangerousTemperature(i);
+                break;
+            }
+        }
+    }
+
+    public void RaiseTemperatureChanged(int temperature)
+    {
+        TemperatureChanged?.Invoke(this, new TemperatureEventArgs { Message = $"Temperature update: {temperature}℃" });
+    }
+
+    public void RaiseDangerousTemperature(int dangerousTemperature)
+    {
+        DangerousTemperature?.Invoke(this, new TemperatureEventArgs { Message = $"WARNING: Dangerous temperature of {dangerousTemperature}℃ reached!" });
+    }
+
+}
