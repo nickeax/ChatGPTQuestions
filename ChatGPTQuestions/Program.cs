a27e55860d75a98470d2f838e3b1127c23a59fa1
Nick Fletcher <nickeax@gmail.com>
2025-04-13 13:35:52 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,16 @@
 ﻿/*
- 
+ 8. Extension Methods
+Question: Write an extension method for the string class that returns the string in reverse order. 
+Then, use this method on a string object to print the reversed string.
  */
+
+var testWord = "esreveR";
+Console.WriteLine(testWord.NFReverse());
+
+public static class StringExtentions
+{
+    public static string NFReverse(this string input)
+    {
+        return string.Join("", input.Reverse());
+    }
+}
