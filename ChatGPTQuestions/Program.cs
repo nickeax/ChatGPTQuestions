bd5522994c379e0d1def9187d0eb10d6ae4c8da3
Nick Fletcher <nickeax@gmail.com>
2025-04-13 13:14:43 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -4,3 +4,13 @@ Question: Write an asynchronous method that downloads data from a URL and prints
 Simulate a delay using Task.Delay if you don't actually want to make a network request.
  */
 
+var res = (await GetNetworkWords());
+Console.WriteLine(string.Join("", res.Take(100)));
+
+async Task<string> GetNetworkWords()
+{
+    var res = string.Join("", (await Task.Run(() => Utils.Mocking.RandomWords(50, 3000))));
+    
+    return res;
+
+}
