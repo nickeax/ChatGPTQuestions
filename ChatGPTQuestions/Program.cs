92523194eb5da0efd856364ad0337952c2dfb2c5
Nick Fletcher <nickeax@gmail.com>
2025-04-16 19:07:54 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,32 @@
 ﻿/*
- 
+ 15. Generics in C#
+Question: Write a generic method Swap<T> that takes two parameters of type T and swaps their values. Test the method by swapping an integer and a string.
  */
+
+int a = 123;
+int b = 654;
+string c = "First";
+string d = "Second";
+
+Console.WriteLine($"Before Swap");
+Console.WriteLine($"a: {a}, b: {b}");
+
+Swap(ref a, ref b);
+
+Console.WriteLine($"After Swap");
+Console.WriteLine($"a: {a}, b: {b}");
+
+Console.WriteLine($"Before Swap");
+Console.WriteLine($"c: {c}, d: {d}");
+
+Swap(ref c, ref d);
+
+Console.WriteLine($"After Swap");
+Console.WriteLine($"c: {c}, d: {d}");
+
+static void Swap<T>(ref T v1, ref T v2)
+{
+    var tmp = v1;
+    v1 = v2;
+    v2 = tmp;
+}
