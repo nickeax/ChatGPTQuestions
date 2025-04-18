4aadbcb791322054e45f07679de2a4434ae52f7c
Nick Fletcher <nickeax@gmail.com>
2025-04-18 14:33:25 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,11 @@
 ﻿/*
- 
+18. DateTime Manipulation
+Question: Write a method that calculates the number of days between two DateTime objects. The method should take two dates as input and return the difference in days. 
  */
+
+Console.WriteLine(DateDifference(DateTime.Now.AddDays(-15), DateTime.Now)); 
+
+static int DateDifference(DateTime begin, DateTime end)
+{
+    return (end - begin).Days;
+}
