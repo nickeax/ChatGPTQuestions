d8e65ff7518796714ee21b3e266bb66083675e6a
Nick Fletcher <nickeax@gmail.com>
2025-04-19 10:16:55 +1000

    Exercise complete.

--- ChatGPTQuestions/Program.cs
+++ ChatGPTQuestions/Program.cs
@@ -1,3 +1,15 @@
 ﻿/*
- 
+ 20. Recursive Method
+Question: Write a recursive method that calculates the factorial of a given number. For example, factorial(5) should return 120.
  */
+
+int testNumber = 5;
+
+Console.WriteLine(Factorial(testNumber));
+
+static int Factorial(int input)
+{
+    if (input == 0)
+        return 1;
+    return input * Factorial(input - 1);
+}
